6a17f054203c45aebc3a6b91ee9246eea8aab2ee
Moacir Braga <contato@moacirbrg.org>
2021-11-29 20:12:45 -0300

    #2 react-bootstrap and sass have been added into the project.

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <Button variant="success" onClick={handleClick}>My Button</Button>
     </div>
   );
+
+  function handleClick(): void {
+    alert("Hello");
+  }
 }
 
 export default App;
