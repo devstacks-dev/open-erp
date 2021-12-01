050d2a11a23d84964e0f618f31b57e5813a3dd43
Bruno Hahn <brunophahn@gmail.com>
2021-12-01 00:24:57 -0300

    #2 Configured NavBar for business requirements.

--- webclient/src/components/template/app/parts/topmenu/TopMenu.tsx
+++ webclient/src/components/template/app/parts/topmenu/TopMenu.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 function TopMenu() {
     return (
         <Navbar bg="light" expand="lg">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand>
+                    <img
+                        src="/assets/logo/favicon.svg"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                        alt="Company logo"
+                    />
+                    Company Name
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <Nav.Link href="/">Dashboard</Nav.Link>
+                        <NavDropdown title='Sales'>
+                            <NavDropdown.Item href="sales/list">List sales</NavDropdown.Item>
+                            <NavDropdown.Item href="sales/add">New sale</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            <NavDropdown.Item href="products/list">List products</NavDropdown.Item>
+                            <NavDropdown.Item href="products/add">New product</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title='Inventory'>
+                            <NavDropdown.Item href="material/list">List raw materials</NavDropdown.Item>
+                            <NavDropdown.Item href="material/add">New material</NavDropdown.Item>
+                        </NavDropdown>
+                        <NavDropdown title='Purchases'>
+                            <NavDropdown.Item href="purchase/list">List orders</NavDropdown.Item>
+                            <NavDropdown.Item href="purchase/add">New order</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="supplier/list">List suppliers</NavDropdown.Item>
+                            <NavDropdown.Item href="supplier/add">New supplier</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
+                    <Navbar.Text>
+                        <a href={'/'}>Sign out</a>
+                    </Navbar.Text>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
