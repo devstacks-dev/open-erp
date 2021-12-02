0528632d8a709656cef1910e6df181402f3768db
Bruno Hahn <brunophahn@gmail.com>
2021-12-02 17:11:14 -0300

    #2 Fixed url stacking bug by using absolute path for navbar links.

--- webclient/src/components/template/parts/topmenu/TopMenu.tsx
+++ webclient/src/components/template/parts/topmenu/TopMenu.tsx
@@ -19,22 +19,22 @@ function TopMenu() {
                     <Nav className="me-auto">
                         <Nav.Link href="/">Dashboard</Nav.Link>
                         <NavDropdown title='Sales'>
-                            <NavDropdown.Item href="sales">List sales</NavDropdown.Item>
-                            <NavDropdown.Item href="sales/add">New sale</NavDropdown.Item>
+                            <NavDropdown.Item href="/sales">List sales</NavDropdown.Item>
+                            <NavDropdown.Item href="/sales/add">New sale</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="products">List products</NavDropdown.Item>
-                            <NavDropdown.Item href="products/add">New product</NavDropdown.Item>
+                            <NavDropdown.Item href="/products">List products</NavDropdown.Item>
+                            <NavDropdown.Item href="/products/add">New product</NavDropdown.Item>
                         </NavDropdown>
                         <NavDropdown title='Inventory'>
-                            <NavDropdown.Item href="inventory">List raw materials</NavDropdown.Item>
-                            <NavDropdown.Item href="inventory/add">New material</NavDropdown.Item>
+                            <NavDropdown.Item href="/inventory">List raw materials</NavDropdown.Item>
+                            <NavDropdown.Item href="/inventory/add">New material</NavDropdown.Item>
                         </NavDropdown>
                         <NavDropdown title='Purchases'>
-                            <NavDropdown.Item href="purchase">List orders</NavDropdown.Item>
-                            <NavDropdown.Item href="purchase/add">New order</NavDropdown.Item>
+                            <NavDropdown.Item href="/purchase">List orders</NavDropdown.Item>
+                            <NavDropdown.Item href="/purchase/add">New order</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="supplier">List suppliers</NavDropdown.Item>
-                            <NavDropdown.Item href="supplier/add">New supplier</NavDropdown.Item>
+                            <NavDropdown.Item href="/supplier">List suppliers</NavDropdown.Item>
+                            <NavDropdown.Item href="/supplier/add">New supplier</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                     <Navbar.Text>
