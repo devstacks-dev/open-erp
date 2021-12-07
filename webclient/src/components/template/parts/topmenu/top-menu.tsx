7934c9a1a1fa7342453be42289b4db499d5c3609
Bruno Hahn <brunophahn@gmail.com>
2021-12-06 21:23:49 -0300

    #2 Applied i18n to TopMenu items.

--- webclient/src/components/template/parts/topmenu/top-menu.tsx
+++ webclient/src/components/template/parts/topmenu/top-menu.tsx
@@ -21,28 +21,28 @@ function TopMenu() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/">Dashboard</Nav.Link>
-                        <NavDropdown title={t('Sales')}>
-                            <NavDropdown.Item href="/sales">List sales</NavDropdown.Item>
-                            <NavDropdown.Item href="/sales/add">New sale</NavDropdown.Item>
+                        <Nav.Link href="/">{t('navBar.Dashboard')}</Nav.Link>
+                        <NavDropdown title={t('navBar.Sales')}>
+                            <NavDropdown.Item href="/sales">{t('navBar.List sales')}</NavDropdown.Item>
+                            <NavDropdown.Item href="/sales/add">{t('navBar.New sale')}</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="/products">List products</NavDropdown.Item>
-                            <NavDropdown.Item href="/products/add">New product</NavDropdown.Item>
+                            <NavDropdown.Item href="/products">{t('navBar.List products')}</NavDropdown.Item>
+                            <NavDropdown.Item href="/products/add">{t('navBar.New product')}</NavDropdown.Item>
                         </NavDropdown>
-                        <NavDropdown title='Inventory'>
-                            <NavDropdown.Item href="/inventory">List raw materials</NavDropdown.Item>
-                            <NavDropdown.Item href="/inventory/add">New material</NavDropdown.Item>
+                        <NavDropdown title={t('navBar.Inventory')}>
+                            <NavDropdown.Item href="/inventory">{t('navBar.List raw materials')}</NavDropdown.Item>
+                            <NavDropdown.Item href="/inventory/add">{t('navBar.New material')}</NavDropdown.Item>
                         </NavDropdown>
-                        <NavDropdown title='Purchases'>
-                            <NavDropdown.Item href="/purchase">List orders</NavDropdown.Item>
-                            <NavDropdown.Item href="/purchase/add">New order</NavDropdown.Item>
+                        <NavDropdown title={t('navBar.Purchases')}>
+                            <NavDropdown.Item href="/purchase">{t('navBar.List orders')}</NavDropdown.Item>
+                            <NavDropdown.Item href="/purchase/add">{t('navBar.New order')}</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="/supplier">List suppliers</NavDropdown.Item>
-                            <NavDropdown.Item href="/supplier/add">New supplier</NavDropdown.Item>
+                            <NavDropdown.Item href="/supplier">{t('navBar.List suppliers')}</NavDropdown.Item>
+                            <NavDropdown.Item href="/supplier/add">{t('navBar.New supplier')}</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                     <Navbar.Text>
-                        <a href={'/'}>Sign out</a>
+                        <a href={'/'}>{t('navBar.Sign Out')}</a>
                     </Navbar.Text>
                 </Navbar.Collapse>
             </Container>
--- webclient/src/i18n.ts
+++ webclient/src/i18n.ts
@@ -4,12 +4,44 @@ import { initReactI18next } from 'react-i18next';
 const resources = {
     en: {
         translation: {
-            'Sales': 'Sales'
+            navBar: {
+                'Dashboard': 'Dashboard',
+                'Inventory': 'Inventory',
+                'List orders': 'List orders',
+                'List products': 'List products',
+                'List raw materials': 'List raw materials',
+                'List sales': 'List sales',
+                'List suppliers': 'List suppliers',
+                'New material': 'New material',
+                'New order': 'New order',
+                'New product': 'New product',
+                'New sale': 'New sale',
+                'New supplier': 'New supplier',
+                'Purchases': 'Purchases',
+                'Sales': 'Sales',
+                'Sign Out': 'Sign Out',
+            }
         }
     },
     pt: {
         translation: {
-            'Sales': 'Vendas'
+            navBar: {
+                'Dashboard': 'Painel',
+                'Inventory': 'Estoque',
+                'List orders': 'Listar ordens',
+                'List products': 'Listar produtos',
+                'List raw materials': 'Listar material',
+                'List sales': 'Listar vendas',
+                'List suppliers': 'Listar fornecedores',
+                'New material': 'Novo material',
+                'New order': 'Nova ordem',
+                'New product': 'Novo produto',
+                'New sale': 'Nova venda',
+                'New supplier': 'Novo fornecedor',
+                'Purchases': 'Compras',
+                'Sales': 'Vendas',
+                'Sign Out': 'Sair',
+            }
         }
     }
 };
@@ -18,11 +50,13 @@ i18n
     .use(initReactI18next)
     .init({
         resources,
-        lng: 'en',
+        fallbackLng: 'en',
 
         interpolation: {
             escapeValue: false
         }
     }).then(() => {});
+
+i18n.changeLanguage(navigator.language).then(() => {})
 
 export default i18n;
